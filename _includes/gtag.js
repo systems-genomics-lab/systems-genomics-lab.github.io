3371469ee982709281a593ff897d91cecd7e778d
Systems Genomics Lab <97294206+systems-genomics-lab@users.noreply.github.com>
2022-01-18 23:02:26 +0200

    Update gtag.js

--- _includes/gtag.js
+++ _includes/gtag.js
@@ -1,9 +1,9 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-0G2ZSGTYRK"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-Y0TS6200EY"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-0G2ZSGTYRK');
+  gtag('config', 'G-Y0TS6200EY');
 </script>
