a1c0ae359b0ba95e20efa86c052212dd29ff021c
Systems Genomics Lab <97294206+systems-genomics-lab@users.noreply.github.com>
2022-01-25 22:02:45 +0200

    Update footer.js

--- _includes/footer.js
+++ _includes/footer.js
@@ -3,6 +3,6 @@
   document.write("<div class='p-3 mb-2 bg-light text-dark'>");
   document.write("<hr>");
   document.write("<a href='/'>Home</a> | <a href='/projects'>Projects</a> | <a href='/members'>Members</a> | <a href='/publications'>Publications</a>");
-  document.write("</div>"
+  document.write("</div>");
 
 </script>
